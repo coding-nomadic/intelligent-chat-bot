acb186cb75673834f29383c546001abcd1f28d7b
Tenzin Dawa <tnzngdw@gmail.com>
2024-03-31 20:22:59 -0400

    Delete intelligent-chat-bot directory

--- intelligent-chat-bot/src/main/java/com/example/intelligent/chatbot/constants/ChatConstants.java
+++
@@ -1,9 +0,0 @@
-package com.example.intelligent.chatbot.constants;
-
-public class ChatConstants {
-    public static final String AUTHORIZATION = "Authorization";
-    public static final String OPENAI_MODEL = "${openai.model}";
-    public static final String OPENAI_API_URL = "${openai.api.url}";
-    public static final String OPENAI_API_KEY = "${openai.api.key}";
-    public static final String ERROR_CODE = "102";
-}
